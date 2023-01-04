ed215c864e794a52ce604b49def0a1bef1daf652
joyci <xxjoycixx@gmail.com>
2023-01-04 02:11:41 -0300

    upload

---
+++ article-preview-component-master/app.js
@@ -0,0 +1,7 @@
+function showSocials() {
+    document.querySelector('.share-active').classList.add('active');
+}
+
+function hideSocials() {
+    document.querySelector('.share-active').classList.remove('active');
+}
